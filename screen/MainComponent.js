ad2cd50d642e4753675e7405778cfad18e0a874b
amosdesigns <mail@amosdesigns.net>
2023-11-20 15:59:13 -0500

    update

---
+++ screen/MainComponent.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { CAMPSITES } from "../shared/campsites";
+import DirectoryScreen from "../DirectoryScreen";
+
+const Main = () => {
+  const [] = useState(CAMPSITES);
+};
+export default Main;
