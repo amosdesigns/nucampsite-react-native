a5d6406911e7f68dc5aa9d661e4cd36eea6145c5
amosdesigns <mail@amosdesigns.net>
2023-11-25 12:09:23 -0500

    Components Part 2

---
+++ screens/CampsiteInfoScreen.js
@@ -0,0 +1,7 @@
+import RenderCampsite from "../features/campsites/RenderCampsite";
+
+const CampsiteInfoScreen = ({ campsite }) => {
+  return <RenderCampsite campsite={campsite} />;
+};
+
+export default CampsiteInfoScreen;
