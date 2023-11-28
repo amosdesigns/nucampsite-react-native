916b720ba6f3dec1bb7b5fac0f05625e61c08e2d
amosdesigns <mail@amosdesigns.net>
2023-11-28 14:15:17 -0500

    working home page - end of week one

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,51 @@
-import { Text, View } from 'react-native';
-const HomeScreen = () => {
+import {useState } from 'react';
+import { Text, View, ScrollView } from 'react-native';
+import { Card } from "react-native-elements";
+import {PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
+import { CAMPSITES } from "../shared/campsites";
+
+const FeaturedItem = ({ item }) => {
+  if (item) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Welcome to the Home Screen!</Text>
-        </View>
+      <Card containerStyle={{ padding: 0 }}>
+        <Card.Image source={item.image}>
+          <View style={{
+            justifyContent: "center",
+            flex: 1
+          }}>
+            <Text style={{
+              color: "white",
+              textAlign: "center",
+              fontSize: 20
+            }}>
+              {item.name}
+            </Text>
+          </View>
+        </Card.Image>
+        <Text style={{ margin: 20 }}>{item.description}</Text>
+      </Card>
+    );
+  }
+  return <View />;
+};
+
+
+
+const HomeScreen = () => {
+ const [campsites, setCampsites] = useState(CAMPSITES);
+const [promotions, setPromotions] = useState(PROMOTIONS);
+  const [partners, setPartners] = useState( PARTNERS );
+  const featCampsite = campsites.find( ( item ) => item.featured );
+  const featPromotion = promotions.find((item) => item.featured);
+  const featPartner = partners.find((item) => item.featured);
+  
+    return (
+      <ScrollView>
+        <FeaturedItem item={featCampsite} />
+        <FeaturedItem item={featPromotion} />
+        <FeaturedItem item={featPartner} />
+      </ScrollView>
     );
  };
 export default HomeScreen;
