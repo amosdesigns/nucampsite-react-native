6311184e999b447fb0711bdf442aeaffb0a695c9
amosdesigns <mail@amosdesigns.net>
2023-11-27 19:50:04 -0500

    Navigation Part 1

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,24 +1,27 @@
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, View } from "react-native";
+import { Card } from "react-native-elements";
 
-const RenderCampsite = ( { campsite } ) => { 
-  if ( campsite ) { 
+const RenderCampsite = ({ campsite }) => {
+  if (campsite) {
     return (
       <Card containerStyle={{ padding: 0 }}>
         <Card.Image source={campsite.image}>
           <View style={{ justifyContent: "center", flex: 1 }}>
-            <Text style={{ 
-              color: 'white',
-              fontSize: 20,
-              textAlign: "center",
-             }}>
+            <Text
+              style={{
+                color: "white",
+                fontSize: 20,
+                textAlign: "center",
+                fontWeight: "bold",
+              }}
+            >
               {campsite.name}
             </Text>
           </View>
         </Card.Image>
         <Text style={{ margin: 20 }}>{campsite.description}</Text>
       </Card>
-    ); 
+    );
   }
   return <View />;
 };
