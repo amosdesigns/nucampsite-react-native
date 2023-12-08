0ec1ad603157e56ce4aee4dae95daea0f6b77c06
amosdesigns <mail@amosdesigns.net>
2023-12-08 17:32:28 -0500

    Modal

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -6,23 +6,38 @@ import {
   StyleSheet,
   Switch,
   Button,
+  Modal,
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 
-const ReservationScreen = () => { 
- const [campers, setCampers] = useState(1);
- const [hikeIn, setHikeIn] = useState(false);
- const [date, setDate] = useState(new Date());
-  const [showCalendar, setShowCalendar] = useState( false );
-  
-  const handleReservation = () => { 
+const ReservationScreen = () => {
+  const [campers, setCampers] = useState(1);
+  const [hikeIn, setHikeIn] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const onDateChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShowCalendar(Platform.OS === "ios");
+    setDate(currentDate);
+  };
+
+  const handleReservation = () => {
+    console.log("campers:", campers);
+    console.log("hikeIn:", hikeIn);
+    console.log("date:", date);
+    setShowModal(!showModal);
+  };
+
+  const resetForm = () => {
     setCampers(1);
     setHikeIn(false);
     setDate(new Date());
     setShowCalendar(false);
-
   };
+
   return (
     <ScrollView>
       <View style={styles.formRow}>
@@ -75,6 +90,31 @@ const ReservationScreen = () => {
           accessibilityLabel="Tap me to search for available campsites to reserve"
         />
       </View>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={showModal}
+        onRequestClose={() => setShowModal(!showModal)}
+      >
+        <View style={styles.modal}>
+          <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
+          <Text style={styles.modalText}>Number of Campers: {campers}</Text>
+          <Text style={styles.modalText}>
+            Hike-In?: {hikeIn ? "Yes" : "No"}
+          </Text>
+          <Text style={styles.modalText}>
+            Date: {date.toLocaleDateString("en-US")}
+          </Text>
+          <Button
+            onPress={() => {
+              setShowModal(!showModal);
+              resetForm();
+            }}
+            color="#5637DD"
+            title="Close"
+          />
+        </View>
+      </Modal>
     </ScrollView>
   );
 };
@@ -93,6 +133,22 @@ const styles = StyleSheet.create({
   },
   formItem: {
     flex: 1,
+  },
+  modal: {
+    justifyContent: "center",
+    margin: 20,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    backgroundColor: "#5637DD",
+    textAlign: "center",
+    color: "#fff",
+    marginBottom: 20,
+  },
+  modalText: {
+    fontSize: 18,
+    margin: 10,
   },
 });
 
